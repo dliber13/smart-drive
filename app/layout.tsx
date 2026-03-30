9f608172534d1501c653b0891a52f365b0359b1c
dliber13 <doug.liber@goodautos.com>
2026-03-30 16:32:10 -0500

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Smart Drive",
+  description: "Smart Drive Financial",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
