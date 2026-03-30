b17e53fa7fd482b474a38a9bef7b2748f24eacc5
dliber13 <doug.liber@goodautos.com>
2026-03-30 16:18:42 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div style={{ padding: 40 }}>
       <h1>Smart Drive is Live 🚀</h1>
-      <p>Your system is successfully deployed.</p>
+      <p>Your system deployed successfully.</p>
     </div>
   );
 }
