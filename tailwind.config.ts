9587ee788e5feea52dd50a861b7c3f49f709d4f3
dliber13 <doug.liber@goodautos.com>
2026-04-09 12:00:49 -0500

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,11 @@
-import type { Config } from "tailwindcss";
-const config: Config = { content:["./app/**/*.{js,ts,jsx,tsx,mdx}","./components/**/*.{js,ts,jsx,tsx,mdx}","./hooks/**/*.{js,ts,jsx,tsx,mdx}","./lib/**/*.{js,ts,jsx,tsx,mdx}","./types/**/*.{js,ts,jsx,tsx,mdx}"], theme:{extend:{}}, plugins:[] };
-export default config;
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
